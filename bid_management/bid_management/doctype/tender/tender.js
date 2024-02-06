326e3118cf35ffe3043525f8d6c0dfd35d7daeb0
Himanshu Shivhare <himanshushivhare047@gmail.com>
2024-02-06 22:13:50 +0530

    Delete bid_management directory

--- bid_management/bid_management/doctype/tender/tender.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Himanshu Shivhare and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Tender', {
-	// refresh: function(frm) {
-
-	// }
-});
--- bid_management/bid_management/doctype/tender/test_tender.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Himanshu Shivhare and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTender(FrappeTestCase):
-	pass
